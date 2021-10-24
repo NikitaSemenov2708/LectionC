154d7808b425c91d954671d6fb690f7d2527dbf5
Semenov Nikita <semenov.nikita.95@mail.ru>
2021-10-24 23:30:28 +0300

    добавили example_002 и gitignore

--- example_002/Program.cs
+++ example_002/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.ReadLine();// C помощью нее происходит считывание данных
+//string- место где будут заложены наши данные
+//Console.WriteLine -выводит текст на новую строку
+//Console.Write - выводится на одну строку
+
+Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
